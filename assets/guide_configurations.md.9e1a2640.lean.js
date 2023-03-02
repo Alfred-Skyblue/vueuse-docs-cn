dccc475f3ae5ac7fb3dfcb5b406e7bccb223d10f
Alfred-Skyblue <yangpanteng@gmail.com>
2023-03-02 05:54:16 +0000

    Deploying to gh-pages from @ Alfred-Skyblue/vueuse-docs-cn@c40fb66926f39ace878c08a68f52dd02859677f8 🚀

---
+++ assets/guide_configurations.md.9e1a2640.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,g as l}from"./app.b382afd7.js";const F=JSON.parse('{"title":"配置","description":"","frontmatter":{},"headers":[],"relativePath":"guide/configurations.md","lastUpdated":1677736325000}'),p={name:"guide/configurations.md"},o=l("",14),e=[o];function c(t,r,E,y,i,d){return n(),a("div",null,e)}const h=s(p,[["render",c]]);export{F as __pageData,h as default};
