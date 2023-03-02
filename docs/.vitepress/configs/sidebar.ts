e1c1ba46232203f5d4035ca7961162f9e0352f21
丶远方 <yangpanteng@gmail.com>
2023-03-02 19:34:30 +0800

    docs: update sidebar

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -8,8 +8,67 @@ const guides = [
 ]
 const defaultSidebar = [{ text: '指导', items: guides, link: '/guide/README' }]
 
-const functionSidebar = getSideBar('./docs/functions')
+const thirdSidebar = [
+  {
+    text: '@Head',
+    link: 'https://github.com/vueuse/head#api',
+    items: [
+      {
+        text: 'createHead', link: 'https://github.com/vueuse/head#api',
+      },
+      {
+        text: 'useHead', link: 'https://github.com/vueuse/head#api',
+      },
+    ],
+  },
+  {
+    text: '@Motion',
+    link: 'https://motion.vueuse.org/api/use-element-style.html',
+    items: [
+      {
+        text: 'useElementStyle', link: 'https://motion.vueuse.org/api/use-element-style.html',
+      },
+      {
+        text: 'useElementTransform', link: 'https://motion.vueuse.org/api/use-element-transform.html',
+      },
+      {
+        text: 'useMotion', link: 'https://motion.vueuse.org/api/use-motion.html',
+      },
+      {
+        text: 'useMotionProperties', link: 'https://motion.vueuse.org/api/use-motion-properties.html',
+      },
+      {
+        text: 'useMotionVariants', link: 'https://motion.vueuse.org/api/use-motion-variants.html',
+      },
+      {
+        text: 'useSpring', link: 'https://motion.vueuse.org/api/use-spring.html',
+      },
+    ],
+  },
+  {
+    text: '@SchemaOrg',
+    link: 'https://vue-schema-org.netlify.app/api/core/create-schema-org.html',
+    items: [
+      {
+        text: 'createSchemaOrg', link: 'https://vue-schema-org.netlify.app/api/core/create-schema-org.html',
+      }, {
+        text: 'useSchemaOrg', link: 'https://vue-schema-org.netlify.app/api/core/use-schema-org.html',
+      },
+    ],
+  },
+  {
+    text: '@Sound',
+    link: 'https://github.com/vueuse/sound#examples',
+    items: [
+      {
+        text: 'useSound', link: 'https://github.com/vueuse/sound#examples',
+      },
+    ],
+  },
 
+]
+
+const functionSidebar = [...getSideBar('./docs/functions'), ...thirdSidebar]
 export const sidebar = {
   '/guide/': defaultSidebar,
   '/functions/': functionSidebar,
