8e1c1cd494bcec406cb7c12a22582ee5eaa9148c
Alfred-Skyblue <yangpanteng@gmail.com>
2023-03-02 11:36:27 +0000

    Deploying to gh-pages from @ Alfred-Skyblue/vueuse-docs-cn@e1c1ba46232203f5d4035ca7961162f9e0352f21 🚀

---
+++ assets/guide_best-practice.md.186a61d5.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,g as l}from"./app.37a78647.js";const F=JSON.parse('{"title":"最佳实践","description":"","frontmatter":{},"headers":[],"relativePath":"guide/best-practice.md","lastUpdated":1677756870000}'),p={name:"guide/best-practice.md"},o=l("",27),e=[o];function c(t,r,E,y,i,d){return n(),a("div",null,e)}const g=s(p,[["render",c]]);export{F as __pageData,g as default};
