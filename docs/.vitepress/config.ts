99120cc6b43bdc3f3673360d065b1cb32adebd89
丶远方 <yangpanteng@gmail.com>
2023-03-06 13:00:13 +0800

    docs: seo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/vueuse-docs-cn/favicon.svg' }],
     ['meta', { name: 'baidu-site-verification', content: 'codeva-0hoO4Rl4KS' }],
+    ['meta', { name: 'google-site-verification', content: 'v3_UHXg-hHfXvTVPh5bhX6_AT-K2oX4AZ8LqbdCzLrM' }],
   ],
-
   markdown: {
     headers: {
       level: [0, 0],
